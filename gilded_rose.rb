9967ac519d1f34cefbf7a3032c4c036c5bf6551d
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2022-09-30 14:38:27 -0400

    Created classes for each item

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,47 +1,26 @@
 class GildedRose
 
   def initialize(items)
-    @items = items
-  end
-
-  def update_quality()
-    @items.each do |item|
+    @items = items.map do |item|
       case item.name
       when "Sulfuras, Hand of Ragnaros"
-        return
+        Sulfuras.new(item)
       when "Backstage passes to a TAFKAL80ETC concert"
-        if item.sell_in <= 0
-          item.quality = 0
-        elsif item.sell_in <= 5
-          item.quality = change_quality(item, 3)
-        elsif item.sell_in <= 10
-          item.quality = change_quality(item, 2)
-        else
-          item.quality = change_quality(item, 1)
-        end
+        BackstagePass.new(item)
       when "Aged Brie"
-        item.quality = change_quality(item, 1)
+        AgedBrie.new(item)
       when "Conjured"
-        item.quality = change_quality(item, -2)
+        ConjuredItem.new(item)
       else
-        if item.sell_in > 0
-          item.quality = change_quality(item, -1)
-        else
-          item.quality = change_quality(item, -2)
-        end
+        CommonItem.new(item)
       end
     end
   end
 
-end
-
-def change_quality(item, value)
-  if item.quality + value >= 50
-      return 50
-  elsif item.quality + value <= 0
-      return 0
-  else
-      return item.quality + value
+  def update_quality()
+    @items.each do |item|
+      item.update
+    end
   end
 end
 
@@ -56,5 +35,66 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class CommonItem < Item
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    if(item.sell_in > 0)
+      item.quality = change_quality(item, -1)
+    else
+      item.quality = change_quality(item, -2)
+    end
+  end
+
+  def change_quality(item, value)
+    if item.quality + value >= 50
+      return 50
+    elsif item.quality + value <= 0
+      return 0
+    else
+      return item.quality + value
+    end
+  end
+end
+
+class AgedBrie < CommonItem
+
+  def update
+    item.quality = change_quality(item, 1)
+  end
+end
+
+class Sulfuras < CommonItem
+
+  def update
+  end
+end
+
+class BackstagePass < CommonItem
+
+  def update
+    if item.sell_in <= 0
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.quality = change_quality(item, 3)
+    elsif item.sell_in <= 10
+      item.quality = change_quality(item, 2)
+    else
+      item.quality = change_quality(item, 1)
+    end
+  end
+end
+
+class ConjuredItem < CommonItem
+
+  def update
+    item.quality = change_quality(item, -2)
   end
 end
